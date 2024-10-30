84d62598ba0910ae1b0fa4667d8bc468650b5629
hnn.ynh <hnnynh125@gmail.com>
2024-10-30 16:28:53 +0900

    fix: add end s3 policy

--- modules/engineers/policy.tf
+++ modules/engineers/policy.tf
@@ -63,7 +63,8 @@ resource "aws_iam_group_policy" "eng_group_policy" {
 				"aws-portal:ModifyBilling",
 				"billing:*",
 				"internetmonitor:*",
-				"kms:*"
+				"kms:*",
+				"s3:*"
 			],
 			"Resource": "*"
 		}
