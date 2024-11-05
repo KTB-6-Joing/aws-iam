8fe07954c0ccff8ccd1a5ea63e70c4a49ee2d887
hnn.ynh <hnnynh125@gmail.com>
2024-11-05 11:07:07 +0900

    fix: add eng acm-pca, tag policy

--- modules/engineers/policy.tf
+++ modules/engineers/policy.tf
@@ -64,7 +64,9 @@ resource "aws_iam_group_policy" "eng_group_policy" {
 				"internetmonitor:*",
 				"kms:*",
 				"s3:*",
-				"acm:*"
+				"acm:*",
+				"acm-pca:*",
+				"tag:*"
 			],
 			"Resource": "*"
 		}
